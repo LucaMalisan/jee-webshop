6912ca80850fcfccf91c2fd55151a75746049be1
luca.malisan <luca.malisan@informaticon.com>
2025-09-13 10:26:51 +0200

    adjust to test data

--- src/main/java/src/model/Article.java
+++ src/main/java/src/model/Article.java
@@ -3,7 +3,7 @@ package src.model;
 import jakarta.persistence.*;
 import java.io.Serializable;
 import java.util.List;
-import java.util.UUID;
+import java.util.Optional;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,28 +13,49 @@ import lombok.Setter;
 @Table(name = "article")
 public class Article implements Serializable {
 
-    @Id
-    @Column(name = "sku", updatable = false, nullable = false)
-    private long sku;
+  @Id
+  @Column(name = "sku", updatable = false, nullable = false)
+  private long sku;
 
-    @Column(name = "title")
-    private String title;
+  @Column(name = "title")
+  private String title;
 
-    @Column(name = "description")
-    private String description;
+  @Column(name = "description")
+  private String description;
 
-    @Column(name = "sellingPrice")
-    private Double sellingPrice;
+  @Column(name = "sellingPrice")
+  private Double sellingPrice;
 
-    @Column(name = "listPrice")
-    private Double listPrice;
+  @Column(name = "listPrice")
+  private Double listPrice;
 
-    @Column(name = "available")
-    private Boolean available;
+  @Column(name = "available")
+  private Boolean available;
 
-    @Column(name = "category")
-    private String category;
+  @Column(name = "category")
+  private String category;
 
-    @OneToMany(mappedBy = "article")
-    private List<ArticleImage> imageList;
+  @OneToMany(mappedBy = "article")
+  private List<ArticleImage> imageList;
+
+  public String getDiscountPercent() {
+    if (this.getListPrice() == null) {
+      return null;
+    }
+
+    return Math.round((100 - (this.getSellingPrice() / this.getListPrice()) * 100)) + "";
+  }
+
+  public String formatPrice() {
+    if (this.getListPrice() != null) {
+      return String.format("%.2f was %.2f CHF", this.getSellingPrice(), this.getListPrice());
+    }
+    return String.format("%.2f CHF", this.getSellingPrice());
+  }
+
+  public String getPrimaryImageURL() {
+    Optional<ArticleImage> optImage =
+        this.getImageList().stream().filter(e -> e.getPosition() == 1).findAny();
+    return optImage.map(ArticleImage::getImageURL).orElse("");
+  }
 }
